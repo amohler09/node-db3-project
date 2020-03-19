3cbcf9dc9750cdeee41802bee7804bb35b126c11
Amber Mohler <ambermohler@iMac.local>
2020-03-18 21:22:06 -0400

    completed find, findById, and findSteps helpers

---
+++ schemes/scheme-model.js
@@ -0,0 +1,33 @@
+const db = require("../data/db-config");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  getSteps
+  //   add,
+  //   update,
+  //   remove
+};
+
+function find() {
+  return db("schemes");
+}
+
+function getSteps() {
+  return db("steps");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+function findSteps(schemeId) {
+  return db("schemes")
+    .join("steps as s", "s.scheme_id", "schemes.id")
+    .select("schemes.scheme_name", "s.instructions", "s.step_number")
+    .where({ scheme_id: schemeId })
+    .orderBy("s.step_number");
+}
